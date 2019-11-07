cd493cda1b695f0ecfcbf79cb9616f5a16996258
David Tauraso <david318@me.com>
2019-11-06 20:55:21 -0800

    Added the video tag

--- src/Photo/NasaPhoto.js
+++ src/Photo/NasaPhoto.js
@@ -4,6 +4,21 @@ import "../App.css";
 
 function NasaPhoto(props)  {
 
+    const photoOrVideo = (props) => {
+
+        if(props.nasaData.media_type === "image"){
+            return (<img className="nasaPhoto" src={props.nasaData.hdurl}></img>);
+
+        } else {
+            return (
+                <video max-width="900">
+                    <source src={props.nasaData.hdurl} type={props.nasaData.media_type} />
+                    Your browser does not support HTML5 video.
+                    </video>
+            )
+        }
+    }
+
     if(!props.nasaData)
     {
         return <p>Loading...</p>;
@@ -15,7 +30,7 @@ function NasaPhoto(props)  {
             <div className="photoCard">
     
                 <p><span className="description">Name: </span>{props.nasaData.title}</p>
-                <img className="nasaPhoto" src={props.nasaData.hdurl}></img>
+                {photoOrVideo(props)}
     
                 <p><span className="description">Description: </span>{props.nasaData.explanation}</p>
     
